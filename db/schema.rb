5f85642367a48ae56f1d507879a0817dc9a50751
Eli Sachs <easachs13@gmail.com>
2022-09-30 17:00:37 -0600

    Scaffold posts

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_930_071_150) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2022_09_30_225414) do
 
-  create_table 'sharks', force: :cascade do |t|
-    t.string 'name'
-    t.text 'facts'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.bigint "shark_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shark_id"], name: "index_posts_on_shark_id"
   end
+
+  create_table "sharks", force: :cascade do |t|
+    t.string "name"
+    t.text "facts"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "sharks"
 end
