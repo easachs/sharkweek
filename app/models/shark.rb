e2152b12ab0d94a670b0799df0ad61befcae633d
Eli Sachs <easachs13@gmail.com>
2022-09-30 17:10:34 -0600

    Modify posts routes/controller

--- app/models/shark.rb
+++ app/models/shark.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Shark < ApplicationRecord
+  has_many :posts, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates_presence_of :facts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts
-  resources :sharks
+  resources :sharks do
+    resources :posts
+  end
   root 'sharks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
