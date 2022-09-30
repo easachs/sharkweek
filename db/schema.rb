5e199a48c7b840a834121fb518ca98b3c6e1910a
Eli Sachs <easachs13@gmail.com>
2022-09-30 17:10:54 -0600

    Rubocop lint

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,25 +12,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_225414) do
-
+ActiveRecord::Schema.define(version: 20_220_930_225_414) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "posts", force: :cascade do |t|
-    t.text "body"
-    t.bigint "shark_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shark_id"], name: "index_posts_on_shark_id"
+  create_table 'posts', force: :cascade do |t|
+    t.text 'body'
+    t.bigint 'shark_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['shark_id'], name: 'index_posts_on_shark_id'
   end
 
-  create_table "sharks", force: :cascade do |t|
-    t.string "name"
-    t.text "facts"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'sharks', force: :cascade do |t|
+    t.string 'name'
+    t.text 'facts'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "posts", "sharks"
+  add_foreign_key 'posts', 'sharks'
 end
